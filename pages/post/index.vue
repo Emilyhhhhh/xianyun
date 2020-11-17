e3ce0aa4e445aaa014e71e73ab61cf111aeb1a9e
xiaobai <123456>
2020-11-17 13:01:37 +0800

    旅游攻略首页的搜索框功能

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="container" title="旅游攻略模块首页文件">
     
+    <search/>
     <el-button type="primary" icon="el-icon-edit" @click="$router.push({path:'/post/create'})">写游记</el-button>
   </div>
 </template>
 
 <script>
+import search from '@/components/post/search.vue'
 export default {
+  components: { search },
 
 }
 </script>
