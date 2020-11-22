161191f8c02ef27db22878048de26d06306347cd
AVGLee <244277187@qq.com>
2020-11-23 01:01:42 +0800

    对500的处理

---
+++ pages/user/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container" style="font-size: 24px">
+    服务器又梦游了
+    <el-button type="success" @click="$router.go(-1)" style="margin-left: 20px"
+      >点击重新加载页面</el-button
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+  
+<style>
+</style>
--- plugins/axios.js
+++ plugins/axios.js
@@ -30,12 +30,9 @@ export default function ({ $axios, redirect, store }) {
         const token = store.state.userstore.userInfo.token
 
         if (token) {
-            // console.log('nuxt/axios onRequest', store.state.userstore.userInfo.token);
             if (checkUrl(config.url)) {
                 config.headers.Authorization = 'Bearer ' + token
             }
-            // console.log(config)
-
         }
     });
 
@@ -44,16 +41,20 @@ export default function ({ $axios, redirect, store }) {
     // 里面可以拦截到错误
     $axios.onError(err => {
         // 这里是每当出错的时候都会被拦截
-        console.log(err);
+        // console.log(err);
         // 这个 err 是一个错误对象, log 的时候看不出来有什么用
         // 用 dir 可以看到里面的内容
-        console.dir(err);
+        // console.dir(err);
         console.log('错误信息是nuxt/axios: ' + err.response.data.message);
         // 弹出一个提示告诉用户
         // this.$message.error('') 在这里由于不是组件内部, 没有 this 可以用
         // 可以单独引入element ui 当中的 Message 组件
         Message.error(err.response.data.message || '系统错误')
+        console.log(err.response.status);
+        if (err.response.status == 500) {
+            redirect('/user/error')
 
+        }
         // 如果需要跳转页面, 比如说需要跳转到登录页
         // 可以从 nuxt 中拿到 redirect 函数即可
         // redirect('/user/login')
